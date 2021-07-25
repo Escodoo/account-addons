f7fd193bf305802447aec21c75e075f76f9bc86c
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-07-25 02:08:30 -0300

    [ADD] account_enable_anglo_saxon_accounting_config

---
+++ account_enable_anglo_saxon_accounting_config/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Enable Anglo Saxon Accounting Config',
+    'summary': """
+        Enable Anglo Saxon Accounting Method on Company Config""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'website': 'https://www.escodoo.com.br',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'views/res_company.xml',
+    ],
+    'demo': [
+    ],
+}
---
+++ account_enable_anglo_saxon_accounting_config/models/res_company.py
@@ -0,0 +1,9 @@
+# Copyright 2021 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+
+
+class ResCompany(models.Model):
+
+    _inherit = 'res.company'
