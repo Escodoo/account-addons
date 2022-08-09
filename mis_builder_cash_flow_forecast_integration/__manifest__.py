ea757353ce58d4d34bdd1152d44b8137440fb4e3
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-09 18:43:00 -0300

    [ADD] mis_builder_cash_flow_forecast_integration

---
+++ mis_builder_cash_flow_forecast_integration/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mis Builder Cash Flow Forecast Integration",
+    "summary": """
+        MIS Builder Cash Flow Forecast Integration""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/account-addons",
+    "depends": [
+        "mis_builder_cash_flow",
+    ],
+    "data": [
+        "views/mis_cash_flow_forecast_line.xml",
+    ],
+    "demo": [],
+}
