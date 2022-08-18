eb7749a969315538fd2156d57eaeca84f7185835
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-18 14:36:06 -0300

    [RFC] mis_builder_cash_flow_forecast_sale: refactor

---
+++ mis_builder_cash_flow_forecast_sale/models/account_invoice.py
@@ -0,0 +1,17 @@
+# Copyright 2022 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class AccountInvoice(models.Model):
+
+    _inherit = "account.invoice"
+
+    def invoice_validate(self):
+        res = super().invoice_validate()
+        for invoice in self:
+            order = invoice.invoice_line_ids.mapped("sale_line_ids.order_id")
+            if order and order.company_id.enable_sale_mis_cash_flow_forecast:
+                order.with_delay()._generate_mis_cash_flow_forecast_lines()
+        return res
