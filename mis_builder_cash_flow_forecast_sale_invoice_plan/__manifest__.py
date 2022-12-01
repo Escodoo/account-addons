9976f70a9376f57078df2fb3e69d3b14b78fb1e4
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-12-01 19:15:41 -0300

    [MIG] mis_builder_cash_flow_forecast_sale_invoice_plan: Migration to 14.0

--- mis_builder_cash_flow_forecast_sale_invoice_plan/__manifest__.py
+++ mis_builder_cash_flow_forecast_sale_invoice_plan/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Mis Builder Cash Flow Forecast Sale Invoice Plan",
     "summary": """
         MIS_Builder Cash Flow Forecast Sale Invoice Plan""",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo",
     "website": "https://github.com/Escodoo/account-addons",
