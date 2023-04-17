067b2ec156c8aa465147bfe26bcc4bca5e300b30
Kaynnan Bardauil <kaynnanx1@gmail.com>
2023-04-17 12:43:48 -0300

    [MIG] sync_multi_company_bank_account: Migration to 15.0

--- sync_multi_company_bank_account/__manifest__.py
+++ sync_multi_company_bank_account/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Sync Multi Company Bank Account",
     "summary": """
         Sync Multi Company Bank Accounts""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo",
     "website": "https://github.com/Escodoo/account-addons",
