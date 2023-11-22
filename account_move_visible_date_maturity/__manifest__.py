91bce461e391a1a8587413ec47f533a358700468
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-11-22 14:08:53 -0300

    [ADD] account_move_visible_date_maturity

---
+++ account_move_visible_date_maturity/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Move Visible Date Maturity",
+    "summary": """
+        Account Move Visible Date Maturity""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/account-addons",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_move.xml",
+    ],
+    "demo": [],
+}
