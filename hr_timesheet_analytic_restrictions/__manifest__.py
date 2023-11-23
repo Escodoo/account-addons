fe0077c1c5acc71b932f64e87a3cbc954545ee2b
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-11-23 14:06:55 -0300

    [ADD] hr_timesheet__analytic_restriction: add new module

---
+++ hr_timesheet_analytic_restrictions/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Hr Timesheet Analytic Restrictions",
+    "summary": """
+        HR Timesheet Analytic Restrictions""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/account-addons",
+    "depends": ["hr_timesheet"],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+}
