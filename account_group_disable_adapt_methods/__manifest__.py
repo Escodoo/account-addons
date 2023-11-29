92e7e1ad3d3a93f00077dfb2dab783ac7a2c3d9e
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-11-29 14:01:18 -0300

    [ADD] account_group_disable_adapt_methods: add new module

---
+++ account_group_disable_adapt_methods/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Group Disable Adapt Methods",
+    "summary": """
+        Account Group Disable Adapt Methods""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/account-addons",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_group.xml",
+    ],
+    "demo": [],
+}
---
+++ account_group_disable_adapt_methods/readme/USAGE.rst
@@ -0,0 +1,4 @@
+#. Install this module.
+#. Update account.group data
+#. After updating the data, it is possible to select the account groups that will be updated and click on the actions button and execute the "Run Adapt Methods" action so that the adapt methods are executed.
+#. After executing the adapt methods of all necessary groups, it is recommended to uninstall this module and only reinstall it again in a major update of account.group.
