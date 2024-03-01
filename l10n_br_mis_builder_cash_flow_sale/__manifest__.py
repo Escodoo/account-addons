7887894f529dc354b280988f51d7edb0363b5def
Kaynnan Lemes <kaynnanx1@gmail.com>
2024-03-01 15:30:10 -0300

    [ADD] l10n_br_mis_builder_cash_flow_sale: add new module

---
+++ l10n_br_mis_builder_cash_flow_sale/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Brazilian Localization Sale and MIS Builder Cash Flow Forecast Sale",
+    "summary": """Customization of MIS Builder Cash Flow Forecast Sale
+        module for implementations in Brazil.""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/account-addons",
+    "depends": ["l10n_br_sale", "mis_builder_cash_flow_forecast_sale"],
+}
