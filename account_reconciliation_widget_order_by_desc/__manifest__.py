0aba168576935687afcf9ca8fd743ce1cf1848e8
Kaynnan Lemes <kaynnanx1@gmail.com>
2024-05-03 17:40:29 -0300

    [ADD] account_reconciliation_widget_order_by_desc: add new module

---
+++ account_reconciliation_widget_order_by_desc/__manifest__.py
@@ -0,0 +1,11 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Reconciliation Widget Order By DESC",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/account-addons",
+    "depends": ["account_reconciliation_widget"],
+}
