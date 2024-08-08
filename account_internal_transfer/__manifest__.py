52ebe6e0bb24cf0905ed4e69ef75bcfad76723de
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2024-08-08 09:53:49 -0300

    [ADD] account_internal_transfer: add new module

---
+++ account_internal_transfer/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Internal Transfer",
+    "summary": """
+        Account Internal Transfer""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/account-addons",
+    "depends": ["account", "account_menu"],
+    "data": [
+        "views/account_internal_transfer_views.xml",
+        "views/res_config_settings.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+}
