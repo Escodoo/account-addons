f6bc8f97661af023d4750e0a49e37303e3565b3a
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2024-11-08 16:55:01 -0300

    [ADD] mis_builder_custom_reports: add new module

---
+++ mis_builder_custom_reports/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MIS Builder Custom Reports",
+    "summary": """
+        MIS Builder Custom Reports""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/account-addons",
+    "depends": ["mis_builder"],
+    "data": [
+        "views/mis_report.xml",
+        "report/mis_report_instance_qweb.xml",
+    ],
+    "demo": [],
+}
