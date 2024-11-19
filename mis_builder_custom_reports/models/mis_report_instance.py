ddb5d6f2c8243c3e3e7a76a21ee8227cdcdced8b
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2024-11-19 10:46:40 -0300

    [IMP] mis_builder_custom_reports: make label optional in report

---
+++ mis_builder_custom_reports/models/mis_report_instance.py
@@ -0,0 +1,11 @@
+# Copyright 2024 - TODAY, Wesley Oliveira <wesley.oliveira@escodoo.com.br>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class MisReportInstance(models.Model):
+
+    _inherit = "mis.report.instance"
+
+    is_round_numbers = fields.Boolean(default=False)
