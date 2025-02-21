49dd0ec6101455a9c9e64d74bbd11b05a8749454
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2025-02-21 17:48:04 -0300

    [ADD] account_statement_import_ofx_unique_import_id: add new module

---
+++ account_statement_import_ofx_unique_import_id/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Statement Import Ofx Unique Import Id",
+    "summary": """
+        Account Statement Import OFX Unique Import ID""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/account-addons",
+    "depends": ["account_statement_import_ofx"],
+    "data": [],
+    "demo": [],
+}
