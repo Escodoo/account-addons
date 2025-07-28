04d6cf714fc6fb58ed72c3cdce8014e1f81a9137
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-07-28 08:37:06 -0300

    [MIG] sale_analytic_restrictions: Migration to 16.0

--- sale_analytic_restrictions/__manifest__.py
+++ sale_analytic_restrictions/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Sale Analytic Restrictions",
     "summary": """
         Sale Analylitic Restrictions""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo",
     "website": "https://github.com/Escodoo/account-addons",
