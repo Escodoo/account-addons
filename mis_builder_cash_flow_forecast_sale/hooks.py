babb9672061f5551982ed4c30f063df814050469
Cristiano Mafra Junior <cristiano.mafracontanto@gmail.com>
2025-11-03 09:24:02 -0300

    [MIG] mis_builder_cash_flow_forecast_sale: Migration to 16.0

--- mis_builder_cash_flow_forecast_sale/hooks.py
+++ mis_builder_cash_flow_forecast_sale/hooks.py
@@ -5,7 +5,6 @@ from odoo import SUPERUSER_ID, api
 
 
 def post_init_hook(cr, registry):
-
     env = api.Environment(cr, SUPERUSER_ID, {})
     orders = env["sale.order"].search([("state", "in", ["sale", "done"])])
     if orders:
--- mis_builder_cash_flow_forecast_sale/models/sale_order_line.py
+++ mis_builder_cash_flow_forecast_sale/models/sale_order_line.py
@@ -5,5 +5,4 @@ from odoo import models
 
 
 class SaleOrderLine(models.Model):
-
     _inherit = "sale.order.line"
