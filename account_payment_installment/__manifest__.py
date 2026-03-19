86f7b690e184e4b4ebac0affe449b506558b3418
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2026-03-19 16:07:29 -0300

    [MIG] account_payment_installment: Migration to 18.0

--- account_payment_installment/__manifest__.py
+++ account_payment_installment/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Account Payment Installment",
     "summary": """
         Account Payment Installment""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo",
     "website": "https://github.com/Escodoo/account-addons",
