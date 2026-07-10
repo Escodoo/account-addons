afb0982f5a5b7b7333017b2ad887ab10ed23ff13
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2026-07-10 16:09:48 -0300

    [ADD] l10n_br_fiscal_difal_inside: add new module

---
+++ l10n_br_fiscal_difal_inside/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Br Fiscal Difal Inside",
+    "summary": """
+        Adds an inside-basis calculation mode for ICMS DIFAL and FCP""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/account-addons",
+    "depends": ["l10n_br_fiscal"],
+    "data": [
+        "views/operation_line_view.xml",
+    ],
+}
